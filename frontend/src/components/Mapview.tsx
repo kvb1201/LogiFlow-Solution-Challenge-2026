768bccf8811aebf7c3bac2b540e3178ff23d9eaf
Ojas Srivastava <srivastavaojas454@gmail.com>
2026-04-04 00:56:29 +0530

    feat: add geocoding fallback logic and update map tiles to CARTO dark theme

--- frontend/src/components/Mapview.tsx
+++ frontend/src/components/Mapview.tsx
@@ -93,8 +93,8 @@ export default function MapView({ routes, selectedRoute = 0 }: { routes: Route[]
         style={{ height: '100%', width: '100%' }}
       >
         <TileLayer
-          attribution="© OpenStreetMap"
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+          url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
         />
 
         {/* All routes */}
